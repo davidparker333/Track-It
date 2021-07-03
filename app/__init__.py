1a1ef172f7345ce21faf2b7848b25a2966ffc3a9
davidparker333 <davidparker333@gmail.com>
2021-07-03 09:09:11 -0400

    user model, login init, db init

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -12,6 +14,10 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app)
+    login.init_app(app)
+
+    login.login_view = 'auth.login'
+    login.login_message_category = 'danger'
 
     with app.app_context():
         from app.blueprints.home import bp as home
