d6f13a7ba26f368d1b87fbc025b8e306a9e9bd6f
David Parker <davidparker333@gmail.com>
2021-07-11 16:06:11 -0400

    track bp and erd

---
+++ app/blueprints/track/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+bp=Blueprint('track', __name__, url_prefix='/track')
+
+from . import routes
---
+++ app/blueprints/track/routes.py
@@ -0,0 +1,3 @@
+from . import bp as track
+from app import db
+from flask import render_template, redirect, url_for, flash, request
