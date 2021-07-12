3ef60c72deca0a42b7283cc48da08e219cb8c4b9
David Parker <davidparker333@gmail.com>
2021-07-12 15:32:53 -0400

    new logo, updated all relevant pages. updated spacing on login and register

--- app/blueprints/track/routes.py
+++ app/blueprints/track/routes.py
@@ -1,7 +1,12 @@
+from app.blueprints.track.models import Package
 from . import bp as track
 from app import db
+from .models import Package, Event
 from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, current_user
 
 @track.route('/')
+@login_required
 def index():
-    return render_template('trackHome.html')
+    packages = Package.query.filter(Package.customer_id == current_user.id).all()
+    return render_template('trackHome.html', packages=packages)
