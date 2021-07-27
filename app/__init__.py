5eb47ab26cd64dd9b6cb984fa64d864fbb10af55
David Parker <davidparker333@gmail.com>
2021-07-27 17:34:37 -0400

    using moment to format date

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_moment import Moment
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
+moment = Moment()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -15,6 +17,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
+    moment.init_app(app)
 
     login.login_view = 'auth.login'
     login.login_message_category = 'danger'
